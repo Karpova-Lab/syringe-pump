7ae69bd0029800c112cb80b90ade6d639eb0cf1b
Andy Lustig <alustig3@gmail.com>
2020-01-13 14:28:13 -0500

    Hardware license update

--- Documentation Source/source/conf.py
+++ Documentation Source/source/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Syringe Pump'
-copyright = '2019, Andy Lustig'
+copyright = '2020, Andy Lustig'
 author = 'Andy Lustig'
 
 # The version info for the project you're documenting, acts as replacement for
